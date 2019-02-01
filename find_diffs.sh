eac565c5d65959e3dea811f6e346bfbef90cafe9
Lee Raulin <leeraulin@gmail.com>
2019-02-01 13:49:48 -0500

    No hardcoded dirs.

--- find_diffs.sh
+++ find_diffs.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
+DIR='/tmp/soalogs'
+mkdir -p $DIR/logs $DIR/logs.prev
+
 # Set bash to "expand" pattern to null string instead of pattern if there are 
 # no matches so we can check if directory is empty
 shopt -s nullglob
 
 # Move logs
-cd logs
 echo 'Moving old logs...'
-if [ $(ls -l | wc -l) -ne 0 ]; then   # if the number of files/directories is not 0
-    for f in *; do
-        mv -- "$f" "../logs.prev/"    # move all the files to the logs.prev directory
+if [ $(ls -l $DIR/logs | wc -l) -ne 0 ]; then   # if the number of files/directories is not 0
+    for f in $DIR/logs/*; do
+        mv -- "$f" "$DIR/logs.prev/"    # move all the files to the logs.prev directory
     done
 else
     echo 'No files to move'
@@ -17,25 +19,27 @@ else
 #     exit 126                          # exit with status "The command can't execute"
 fi
 
-cd ../
 echo 'Fetching new logs...'
-sh get_logs.sh                        # get new logs
+cat $1 | while read in; do dig "$in" -t ANY | sed '/^;;/d; /^; EDNS/d; /^; <<>>/d'  > $DIR/logs/"$in"; done
 
-cd logs
+
 echo 'Comparing logs...'
 
-echo "" > ../diffs.log
+echo "" > $DIR/diffs.log
 
+cd $DIR/logs
 if [ $(ls -l ../logs.prev | wc -l) -ne 0 ]; then  
     for f in *; do
         DIFF=$(diff $f ../logs.prev/$f)
         if [ -n "$DIFF" ]; then
-            echo "********************\nDiffs for $f\n********************" >> ../diffs.log
-            diff $f ../logs.prev/$f >> ../diffs.log # compare each log in new folder to log in old folder
+            echo "********************\nDiffs for $f\n********************" >> $DIR/diffs.log
+            diff $f ../logs.prev/$f >> $DIR/diffs.log # compare each log in new folder to log in old folder
         fi                                          # and save differences in file
     done                                            
 else
     echo 'No old logs to compare.'
 fi
+
+cat $DIR/diffs.log
 
 echo 'Done.'
